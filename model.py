0e91588168f1c9ed0d56c29e046ee5183481fd1a
PhoenixBound <phoenixbound.github@gmail.com>
2023-12-20 00:46:26 -0600

    Hack away at the discriminators

--- model.py
+++ model.py
@@ -81,8 +81,7 @@ def generator_coherence_loss(big_x, big_y):
     # big_y = tf.signal.stft(y, 1024, 256)
     
     # need to reduce across only the time dimension; keep separate freqs
-    # which axis is that????
-    numerator = tf.math.reduce_sum(big_x * big_y, axis=0)
+    numerator = tf.math.reduce_sum(big_x * big_y, axis=1)
     numerator = tf.math.abs(numerator)
     numerator = tf.tensordot(numerator, mel_matrix_what, 1)
     denominator = tf.math.reduce_sum(tf.square(big_y))
@@ -97,8 +96,7 @@ def generator_mel_loss(big_x, big_y):
     # big_y = tf.signal.stft(y, 1024, 256)
     y_term = tf.tensordot(tf.square(tf.math.abs(big_y)), mel_matrix_what, 1)
     x_term = tf.tensordot(tf.square(tf.math.abs(big_x)), mel_matrix_what, 1)
-    # Which axis is time frame count (L)?
-    return tf.math.reduce_sum(tf.math.abs(y_term - x_term)) / M / x_term.shape[0]
+    return tf.math.reduce_sum(tf.math.abs(y_term - x_term)) / M / x_term.shape[1]
 
 # Adapted from jik876/hifi-gan, available under the MIT license
 # MIT License
@@ -132,29 +130,29 @@ class PeriodDiscriminator(tf.keras.layers.Layer):
         stride = (3, 1)
         
         self.convs_list = [
-            tf.keras.layers.Reshape((period, -1)),
-            tf.keras.layers.Conv2D(filters=32, kernel_size=kernel_size, strides=stride, padding='same'),
+            tf.keras.layers.Reshape((-1, period, 2)),
+            tf.keras.layers.Conv2D(filters=32, kernel_size=kernel_size, strides=stride, padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
-            tf.keras.layers.Conv2D(filters=128, kernel_size=kernel_size, strides=stride, padding='same'),
+            tf.keras.layers.Conv2D(filters=128, kernel_size=kernel_size, strides=stride, padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
-            tf.keras.layers.Conv2D(filters=512, kernel_size=kernel_size, strides=stride, padding='same'),
+            tf.keras.layers.Conv2D(filters=512, kernel_size=kernel_size, strides=stride, padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
-            tf.keras.layers.Conv2D(filters=1024, kernel_size=kernel_size, strides=stride, padding='same'),
+            tf.keras.layers.Conv2D(filters=1024, kernel_size=kernel_size, strides=stride, padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
-            tf.keras.layers.Conv2D(filters=1024, kernel_size=kernel_size, strides=(1, 1), padding='same'),
+            tf.keras.layers.Conv2D(filters=1024, kernel_size=kernel_size, strides=(1, 1), padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
-            tf.keras.layers.Conv2D(filters=1, kernel_size=(3, 1), strides=(1, 1), padding='same'),
+            tf.keras.layers.Conv2D(filters=1, kernel_size=(3, 1), strides=(1, 1), padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization()
         ]
 
     def build(self, input_shape):
         # Which axis is time frame count (L)?
-        frame_count = input_shape[0]
+        frame_count = input_shape[1]
         period = self.period
         padding = (period - frame_count % period) % period
         self.convs_list.insert(0, tf.keras.layers.ZeroPadding1D(padding))
@@ -177,7 +175,7 @@ class ScaleDiscriminator(tf.keras.layers.Layer):
         
         convs_list = []
         for _ in range(mean_pool_count):
-            convs_list.append(tf.keras.layers.AvgPooling1D(pool_size=4, strides=2, padding='same'))
+            convs_list.append(tf.keras.layers.AveragePooling1D(pool_size=4, strides=2, padding='same'))
         add_normalized_layer(convs_list, tf.keras.layers.Conv1D(filters=128, kernel_size=15, strides=1, padding='same'))
         convs_list.append(tf.keras.layers.LeakyReLU(0.1))
         add_normalized_layer(convs_list, tf.keras.layers.Conv1D(filters=128, kernel_size=41, strides=2, padding='same', groups=4))
@@ -201,6 +199,35 @@ class ScaleDiscriminator(tf.keras.layers.Layer):
 
 # End HiFi-GAN code
 
+class Discriminator(tf.keras.Model):
+    def __init__(self, name=None, **kwargs):
+        super().__init__(**kwargs)
+        self.p2 = PeriodDiscriminator(2)
+        self.p3 = PeriodDiscriminator(3)
+        self.p5 = PeriodDiscriminator(5)
+        self.p7 = PeriodDiscriminator(7)
+        self.p11 = PeriodDiscriminator(11)
+        self.s0 = ScaleDiscriminator(0, True)
+        self.s1 = ScaleDiscriminator(1, False)
+        self.s2 = ScaleDiscriminator(2, False)
+
+    def call(self, inputs):
+        op2 = self.p2(inputs)
+        print(op2.shape)
+        assert False
+        op3 = self.p3(inputs_3d)
+        op5 = self.p5(inputs_3d)
+        op7 = self.p7(inputs_3d)
+        op11 = self.p11(inputs_3d)
+        os0 = self.s0(inputs_3d)
+        print(os0.shape)
+        os1 = self.s1(inputs)
+        os2 = self.s2(inputs)
+        outputs = tf.stack((op2, op3, op5, op7, op11, os0, os1, os2), axis=0)
+        print(outputs.shape)
+        assert False
+        return tf.reduce_mean(outputs, axis=0)
+
 def generator_adversarial_loss_part(d_of_y):
     # The generator only cares about how well it was able to fool the discriminator,
     # not how well the discriminator can identify genuine audio.
@@ -211,28 +238,40 @@ def discriminator_adversarial_loss_part(d_of_x, d_of_y):
     return tf.square(d_of_x - tf.ones_like(d_of_x)) + tf.square(d_of_y)
 
 
+discriminator = Discriminator()
+
 coh_loss_scale = tf.constant(2.5)
 mel_loss_scale = tf.constant(5.625)
 
-generator_optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=1.6e-3, beta_1=0.8, beta_2=0.99)
-discriminator_optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=1.6e-3, beta_1=0.8, beta_2=0.99)
+generator_optimizer = tf.keras.optimizers.AdamW(learning_rate=1.6e-3, beta_1=0.8, beta_2=0.99)
+discriminator_optimizer = tf.keras.optimizers.AdamW(learning_rate=1.6e-3, beta_1=0.8, beta_2=0.99)
 
 def train_step(x):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        big_x_gen_0 = tf.signal.stft(x[:,:,0], 116, 58)
-        big_x_gen_1 = tf.signal.stft(x[:,:,1], 116, 58)
+        x_0 = x[:,:,0]
+        x_1 = x[:,:,1]
+        big_x_gen_0 = tf.signal.stft(x_0, 116, 58)
+        big_x_gen_1 = tf.signal.stft(x_1, 116, 58)
+        big_x_gen = tf.concat((big_x_gen_0, big_x_gen_1), axis=0)
         # Each channel of the audio has an FFT size of (1299, 65)
         # (116 fits into 128, meaning the frequencies range 0-64, and (1300 - 1) * 58 STFT frames could be made)
         # (This includes 160 frames of context that we added in the preprocessing stage.)
-        # Anyway, the shape of big_x_gen_0 is now (batch size, STFT time frames, STFT freq buckets)
-        # big_x_gen = None
-        big_y_gen_0 = generator(big_x_gen_0)
-        big_y_gen = None
-        assert False
-        y = tf.signal.inverse_stft(big_y_gen, 116, 58, window_fn=tf.signal.inverse_stft_window_fn(58))
+        # Anyway, the shape of big_x_gen is now (batch size * 2, STFT time frames, STFT freq buckets)
+        big_y_gen = generator(big_x_gen)
+        y_0 = tf.signal.inverse_stft(big_y_gen[:x.shape[0]], 116, 58, window_fn=tf.signal.inverse_stft_window_fn(58))
+        y_1 = tf.signal.inverse_stft(big_y_gen[x.shape[0]:], 116, 58, window_fn=tf.signal.inverse_stft_window_fn(58))
         
-        big_x = tf.signal.stft(x, 1024, 256)
-        big_y = tf.signal.stft(y, 1024, 256)
+        y = tf.stack((y_0, y_1), axis=2)
+        x_cut = x[:,-66120:]
+        y_cut = y[:,-66120:]
+        
+        x_discrim_output = discriminator(x_cut)
+        y_discrim_output = discriminator(y_cut)
+        
+        big_x_0 = tf.signal.stft(x_0, 1024, 256)
+        big_x_1 = tf.signal.stft(x_1, 1024, 256)
+        big_y_0 = tf.signal.stft(y_0, 1024, 256)
+        big_y_1 = tf.sginal.stft(y_1, 1024, 256)
     assert False
 
 train_step(next(iter(train_ds.take(1))))
