1867a797109f37d3ebf58cedc160f32baba30887
PhoenixBound <phoenixbound.github@gmail.com>
2023-12-21 22:17:07 -0600

    Loss and gradient calculations

--- model.py
+++ model.py
@@ -3,16 +3,13 @@ from pathlib import Path
 
 print("TensorFlow version:", tf.__version__)
 
-
-
-
 # https://www.tensorflow.org/tutorials/audio/simple_audio
 # Run generate_file_list.py and preprocess.py first to create a simple enough audio dataset to easily import
 full_ds = tf.keras.utils.audio_dataset_from_directory('musdb18hq-processed/', class_names=('train', 'validation', 'test'))
 # Get rid of the labels and divide into parts
-train_ds = full_ds.unbatch().filter(lambda _, x: x == 0).map(lambda x, _: x).batch(16)
-val_ds = full_ds.unbatch().filter(lambda _, x: x == 1).map(lambda x, _: x).batch(16)
-test_ds = full_ds.unbatch().filter(lambda _, x: x == 2).map(lambda x, _: x).batch(16)
+train_ds = full_ds.unbatch().filter(lambda _, x: x == 0).map(lambda x, _: x).batch(4)
+val_ds = full_ds.unbatch().filter(lambda _, x: x == 1).map(lambda x, _: x).batch(4)
+test_ds = full_ds.unbatch().filter(lambda _, x: x == 2).map(lambda x, _: x).batch(4)
 
 # Each entry taken from one of these datasets has dimensions (batch_size, time frames, channels)
 # (in this case, (16, 75400, 2) for full batches)
@@ -34,10 +31,8 @@ test_ds = full_ds.unbatch().filter(lambda _, x: x == 2).map(lambda x, _: x).batc
 K = 65
 # Number of mel frequency bands during loss calculation
 M = 80
-relu = tf.keras.activations.relu
 
-
-mel_matrix_what = tf.signal.linear_to_mel_weight_matrix(M, 513, sample_rate=22050)
+mel_xform_matrix = tf.signal.linear_to_mel_weight_matrix(M, 513, sample_rate=22050)
 
 class Complex2Real(tf.keras.layers.Layer):
     def __init__(self):
@@ -64,6 +59,7 @@ class Real2Complex(tf.keras.layers.Layer):
         imag_inputs = inputs[:,:,1::2]
         return tf.complex(real_inputs, imag_inputs)
 
+relu = tf.keras.activations.relu
 generator = tf.keras.Sequential([
     Complex2Real(),
     # Even though they're causal convolutions, we expect the caller to init
@@ -79,24 +75,27 @@ generator = tf.keras.Sequential([
 def generator_coherence_loss(big_x, big_y):
     # big_x = tf.signal.stft(x, 1024, 256)
     # big_y = tf.signal.stft(y, 1024, 256)
+    # The shape of big_x is (batch size * 2, STFT time frames, STFT freq buckets)
     
     # need to reduce across only the time dimension; keep separate freqs
-    numerator = tf.math.reduce_sum(big_x * big_y, axis=1)
-    numerator = tf.math.abs(numerator)
-    numerator = tf.tensordot(numerator, mel_matrix_what, 1)
-    denominator = tf.math.reduce_sum(tf.square(big_y))
-    denominator = denominator * tf.math.reduce_sum(tf.square(big_x))
+    numerator = tf.reduce_sum(big_x * big_y, axis=1)
+    numerator = tf.abs(numerator)
+    print(numerator.shape)
+    numerator = tf.tensordot(numerator, mel_xform_matrix, 1)
+    print(numerator.shape)
+    denominator = tf.reduce_sum(tf.square(tf.abs(big_y)), axis=1)
+    denominator = denominator * tf.reduce_sum(tf.square(tf.abs(big_x)), axis=1)
     denominator = tf.sqrt(denominator)
-    denominator = tf.tensordot(denominator, mel_matrix_what, 1)
+    denominator = tf.tensordot(denominator, mel_xform_matrix, 1)
     # This should be a scalar
     return tf.math.reduce_sum(numerator / denominator) / M
 
 def generator_mel_loss(big_x, big_y):
     # big_x = tf.signal.stft(x, 1024, 256)
     # big_y = tf.signal.stft(y, 1024, 256)
-    y_term = tf.tensordot(tf.square(tf.math.abs(big_y)), mel_matrix_what, 1)
-    x_term = tf.tensordot(tf.square(tf.math.abs(big_x)), mel_matrix_what, 1)
-    return tf.math.reduce_sum(tf.math.abs(y_term - x_term)) / M / x_term.shape[1]
+    y_term = tf.tensordot(tf.square(tf.abs(big_y)), mel_xform_matrix, 1)
+    x_term = tf.tensordot(tf.square(tf.abs(big_x)), mel_xform_matrix, 1)
+    return tf.math.reduce_sum(tf.abs(y_term - x_term)) / M / x_term.shape[1]
 
 # Adapted from jik876/hifi-gan, available under the MIT license
 # MIT License
@@ -130,7 +129,7 @@ class PeriodDiscriminator(tf.keras.layers.Layer):
         stride = (3, 1)
         
         self.convs_list = [
-            tf.keras.layers.Reshape((-1, period, 2)),
+            tf.keras.layers.Reshape((-1, period, 1)),
             tf.keras.layers.Conv2D(filters=32, kernel_size=kernel_size, strides=stride, padding='same', data_format='channels_last'),
             tf.keras.layers.UnitNormalization(),
             tf.keras.layers.LeakyReLU(0.1),
@@ -151,15 +150,19 @@ class PeriodDiscriminator(tf.keras.layers.Layer):
         ]
 
     def build(self, input_shape):
-        # Which axis is time frame count (L)?
+        # Add time frames to the input
         frame_count = input_shape[1]
         period = self.period
         padding = (period - frame_count % period) % period
-        self.convs_list.insert(0, tf.keras.layers.ZeroPadding1D(padding))
+        self.convs_list.insert(0, tf.keras.layers.ZeroPadding1D((0, padding)))
         self.convs = tf.keras.Sequential(self.convs_list)
     
     def call(self, inputs):
-        return self.convs(inputs)
+        output = self.convs(inputs)
+        # I think we want one neat loss number per signal, so... let's smash this all down
+        output = tf.reduce_mean(output, axis=2)
+        output = tf.reduce_mean(output, axis=1)
+        return output
 
 def append_spectral_normalized_layer(layer_list, layer):
     layer_list.append(tf.keras.layers.SpectralNormalization(layer))
@@ -195,7 +198,10 @@ class ScaleDiscriminator(tf.keras.layers.Layer):
         self.convs = tf.keras.Sequential(convs_list)
 
     def call(self, inputs):
-        return self.convs(inputs)
+        output = self.convs(inputs)
+        # Squash it down I guess
+        output = tf.reduce_mean(output, axis=1)
+        return output
 
 # End HiFi-GAN code
 
@@ -213,29 +219,28 @@ class Discriminator(tf.keras.Model):
 
     def call(self, inputs):
         op2 = self.p2(inputs)
-        print(op2.shape)
-        assert False
-        op3 = self.p3(inputs_3d)
-        op5 = self.p5(inputs_3d)
-        op7 = self.p7(inputs_3d)
-        op11 = self.p11(inputs_3d)
-        os0 = self.s0(inputs_3d)
-        print(os0.shape)
+        op3 = self.p3(inputs)
+        op5 = self.p5(inputs)
+        op7 = self.p7(inputs)
+        op11 = self.p11(inputs)
+        os0 = self.s0(inputs)
         os1 = self.s1(inputs)
         os2 = self.s2(inputs)
-        outputs = tf.stack((op2, op3, op5, op7, op11, os0, os1, os2), axis=0)
-        print(outputs.shape)
-        assert False
-        return tf.reduce_mean(outputs, axis=0)
+        outputs = tf.concat((op2, op3, op5, op7, op11, os0, os1, os2), axis=1)
+        return outputs
 
-def generator_adversarial_loss_part(d_of_y):
+def generator_adversarial_loss(d_of_y):
     # The generator only cares about how well it was able to fool the discriminator,
     # not how well the discriminator can identify genuine audio.
-    return tf.square(d_of_y - tf.ones_like(d_of_y))
+    # Discriminator output of 1 = judged to be original
+    # Discriminator output of 0 = judged to be generator output
+    mse = tf.square(d_of_y - tf.ones_like(d_of_y))
+    return tf.reduce_sum(mse) / 8
 
-def discriminator_adversarial_loss_part(d_of_x, d_of_y):
+def discriminator_adversarial_loss(d_of_x, d_of_y):
     # The discriminator cares about both kinds of failures it makes
-    return tf.square(d_of_x - tf.ones_like(d_of_x)) + tf.square(d_of_y)
+    mse = tf.square(d_of_x - tf.ones_like(d_of_x)) + tf.square(d_of_y)
+    return tf.reduce_sum(mse) / 8
 
 
 discriminator = Discriminator()
@@ -247,7 +252,9 @@ generator_optimizer = tf.keras.optimizers.AdamW(learning_rate=1.6e-3, beta_1=0.8
 discriminator_optimizer = tf.keras.optimizers.AdamW(learning_rate=1.6e-3, beta_1=0.8, beta_2=0.99)
 
 def train_step(x):
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    generator_loss = None
+    discrim_loss = None
+    with tf.GradientTape() as gen_tape: # , tf.GradientTape() as disc_tape:
         x_0 = x[:,:,0]
         x_1 = x[:,:,1]
         big_x_gen_0 = tf.signal.stft(x_0, 116, 58)
@@ -261,17 +268,45 @@ def train_step(x):
         y_0 = tf.signal.inverse_stft(big_y_gen[:x.shape[0]], 116, 58, window_fn=tf.signal.inverse_stft_window_fn(58))
         y_1 = tf.signal.inverse_stft(big_y_gen[x.shape[0]:], 116, 58, window_fn=tf.signal.inverse_stft_window_fn(58))
         
-        y = tf.stack((y_0, y_1), axis=2)
-        x_cut = x[:,-66120:]
-        y_cut = y[:,-66120:]
+        # The HiFi-GAN discriminators are meant for mono signals
+        x_cut = tf.concat((x_0[:,-66120:], x_1[:,-66120:]), axis=0)
+        y_cut = tf.concat((y_0[:,-66120:], y_1[:,-66120:]), axis=0)
+        x_cut = x_cut[..., tf.newaxis]
+        y_cut = y_cut[..., tf.newaxis]
         
-        x_discrim_output = discriminator(x_cut)
-        y_discrim_output = discriminator(y_cut)
+        # The discriminators are slow! Don't run them while testing loss calculations
+        # x_discrim_output = discriminator(x_cut)
+        # y_discrim_output = discriminator(y_cut)
         
-        big_x_0 = tf.signal.stft(x_0, 1024, 256)
-        big_x_1 = tf.signal.stft(x_1, 1024, 256)
-        big_y_0 = tf.signal.stft(y_0, 1024, 256)
-        big_y_1 = tf.sginal.stft(y_1, 1024, 256)
-    assert False
+        # Say that the discriminator did its job perfectly
+        # x_discrim_output = tf.ones((x_0.shape[0] * 2, 8))
+        # y_discrim_output = tf.zeros((x_0.shape[0] * 2, 8))
+        
+        # assert x_discrim_output.shape == (x_0.shape[0] * 2, 8)
+        
+        big_x = tf.signal.stft(tf.squeeze(x_cut, [2]), 1024, 256)
+        big_y = tf.signal.stft(tf.squeeze(y_cut, [2]), 1024, 256)
+        
+        coh_loss = generator_coherence_loss(big_x, big_y)
+        mel_loss = generator_mel_loss(big_x, big_y)
+        # adv_loss = generator_adversarial_loss(y_discrim_output)
+        
+        generator_loss = coh_loss * coh_loss_scale + mel_loss * mel_loss_scale # + adv_loss
+        # discrim_loss = discriminator_adversarial_loss(x_discrim_output, y_discrim_output)
 
-train_step(next(iter(train_ds.take(1))))
+    print('Calculating generator gradients')
+    gen_gradients = gen_tape.gradient(generator_loss, generator.trainable_variables)
+    # print('Calculating discriminator gradients')
+    # discrim_gradients = disc_tape.gradient(discrim_loss, discriminator.trainable_variables)
+    print('Applying generator gradients')
+    generator_optimizer.apply_gradients(zip(gen_gradients, generator.trainable_variables))
+    # print('Applying discriminator gradients')
+    # discriminator_optimizer.apply_gradients(zip(discrim_gradients, discriminator.trainable_variables))
+    print('Generator loss was', generator_loss)
+
+thing = next(iter(train_ds.take(1)))
+train_step(thing)
+train_step(thing)
+train_step(thing)
+train_step(thing)
+train_step(thing)
